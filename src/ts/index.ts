c9127f9e0b0b6442eb4a60c444df1d4e33b4dc9b
Mitsuya Watanabe <mitsuya.watanabe85@gmail.com>
2020-11-22 17:57:11 +0100

    Delete unused code.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -14,65 +14,6 @@ if (!!contentsElem) {
 }
 
 window.onload = () => {
-  const data = [1, 5, 9, 10, 2, 54, 23, 90, 81];
-
-  // let sortedData: Array<number> = [];
-  let uniqueData: Set<number> = new Set();
-
-  for (let i = 0; i < 10000000; i++) {
-    const index = Math.floor(Math.random() * 100);
-    uniqueData.add(index);
-  }
-
-  const sortedData: Array<number> = Array.from(uniqueData).sort(
-    (a, b) => a - b
-  );
-
-  let searchIndex = 10;
-
-  console.log("search index", searchIndex);
-
-  // let startTime = performance.now();
-  // let sortedSearchIndex = binarySearch(sortedData, searchIndex);
-  // let endTime = performance.now();
-
-  // console.log(
-  //   "binary serch index",
-  //   sortedSearchIndex,
-  //   "time",
-  //   endTime - startTime
-  // );
-
-  let startTime, endTime;
-
-  startTime = performance.now();
-  let sortedSearchIndex = binarySearch(sortedData, searchIndex);
-  endTime = performance.now();
-
-  console.log(
-    "binary serch index",
-    sortedSearchIndex,
-    "time",
-    endTime - startTime
-  );
-
-  const linearData: Array<number> = Array.from(uniqueData);
-
-  startTime = performance.now();
-  const linearIndex = linearSearch(linearData, searchIndex);
-  endTime = performance.now();
-  console.log("linear serch index", linearIndex, "time", endTime - startTime);
-
-  startTime = performance.now();
-  sortedSearchIndex = binarySearch(sortedData, searchIndex);
-  endTime = performance.now();
-
-  console.log(
-    "binary serch index",
-    sortedSearchIndex,
-    "time",
-    endTime - startTime
-  );
   setup();
   draw();
 };
@@ -99,39 +40,4 @@ const draw = () => {
 
   particleSystem.render();
   window.requestAnimationFrame(draw);
-};
-
-const linearSearch = (arr: Array<number>, target: number): number => {
-  let index = -1;
-  for (let i = 0; i < arr.length; i++) {
-    const current = arr[i];
-
-    if (current === target) {
-      index = i;
-      break;
-    }
-  }
-
-  return index;
-};
-
-const binarySearch = (arr: Array<number>, target: number): number => {
-  let index = -1;
-  let left = 0;
-  let right = arr.length - 1;
-  let middle = 0;
-  while (left <= right) {
-    middle = Math.floor((left + right) / 2);
-
-    if (arr[middle] === target) {
-      index = middle;
-      break;
-    } else if (arr[middle] < target) {
-      left = middle + 1;
-    } else {
-      right = middle - 1;
-    }
-  }
-
-  return index;
 };
